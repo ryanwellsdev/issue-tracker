825e1c4b10f8d2795733afd6e982373a1ac2de28
ryanwellsdev <ryanwellsdev@gmail.com>
2024-06-07 08:37:23 -0600

    Fix Caching Issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -40,6 +40,7 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
       if (issue) axios.patch("/api/issues/" + issue.id, data);
       else await axios.post("/api/issues", data);
       router.push("/issues");
+      router.refresh();
     } catch (error) {
       setSubmitting(false);
       console.log(error);
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,37 +1,41 @@
-import prisma from '@/prisma/client';
-import { Table } from '@radix-ui/themes';
-import {IssueStatusBadge, Link,  } from '@/app/components'
-import IssueToolbar from './IssueToolbar';
+import prisma from "@/prisma/client";
+import { Table } from "@radix-ui/themes";
+import { IssueStatusBadge, Link } from "@/app/components";
+import IssueToolbar from "./IssueToolbar";
 
 const IssuesPage = async () => {
   const issue = await prisma.issue.findMany();
 
   return (
-    <div> 
+    <div>
       <IssueToolbar />
-      <Table.Root variant='surface'>
+      <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
             <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className='hidden md:table-cell'>Status</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className='hidden md:table-cell'>Created</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Status
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Created
+            </Table.ColumnHeaderCell>
           </Table.Row>
         </Table.Header>
         <Table.Body>
-          {issue.map(issue => (
+          {issue.map((issue) => (
             <Table.Row key={issue.id}>
               <Table.Cell>
-                <Link href={`/issues/${issue.id}`} >
-                {issue.title}
-                </Link>
-                <div className='block md:hidden'>
+                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
+                <div className="block md:hidden">
                   <IssueStatusBadge status={issue.status} />
-                  </div>
+                </div>
               </Table.Cell>
-              <Table.Cell className='hidden md:table-cell'>
+              <Table.Cell className="hidden md:table-cell">
                 <IssueStatusBadge status={issue.status} />
-                </Table.Cell>
-              <Table.Cell className='hidden md:table-cell'>{issue.createdAt.toDateString()}</Table.Cell>
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {issue.createdAt.toDateString()}
+              </Table.Cell>
             </Table.Row>
           ))}
         </Table.Body>
@@ -39,5 +43,7 @@ const IssuesPage = async () => {
     </div>
   );
 };
+
+export const dynamic = "force-dynamic";
 
 export default IssuesPage;
