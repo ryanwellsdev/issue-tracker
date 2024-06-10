487a2931efbbc4bc1ce0c4897f100fd0ec3b7607
ryanwellsdev <ryanwellsdev@gmail.com>
2024-06-10 08:41:43 -0600

    Add a Delete Button

--- app/issues/[id]/IssueDetails.tsx
+++ app/issues/[id]/IssueDetails.tsx
@@ -1,21 +1,21 @@
-import { IssueStatusBadge } from '@/app/components'
-import { Issue } from '@prisma/client'
-import { Card, Flex, Heading, Text } from '@radix-ui/themes'
-import ReactMarkdown from 'react-markdown'
+import { IssueStatusBadge } from "@/app/components";
+import { Issue } from "@prisma/client";
+import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import ReactMarkdown from "react-markdown";
 
-const IssueDetails = ({issue}: { issue: Issue}) => {
+const IssueDetails = ({ issue }: { issue: Issue }) => {
   return (
     <div>
-    <Heading>{issue.title}</Heading>
-        <Flex gap={'3'} my={'2'}>
+      <Heading>{issue.title}</Heading>
+      <Flex gap={"3"} my={"2"}>
         <IssueStatusBadge status={issue.status} />
         <Text>{issue.createdAt.toDateString()}</Text>
-        </Flex>
-        <Card className='prose' mt={'4'}>
+      </Flex>
+      <Card className="prose max-w-full" mt={"4"}>
         <ReactMarkdown>{issue.description}</ReactMarkdown>
-        </Card>
+      </Card>
     </div>
-  )
-}
+  );
+};
 
-export default IssueDetails
+export default IssueDetails;
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,37 +1,36 @@
-import prisma from '@/prisma/client'
-import { Box, Grid } from '@radix-ui/themes'
-import { notFound } from 'next/navigation'
-import EditIssueButton from './EditIssueButton'
-import IssueDetails from './IssueDetails'
-
+import prisma from "@/prisma/client";
+import { Box, Flex, Grid } from "@radix-ui/themes";
+import { notFound } from "next/navigation";
+import EditIssueButton from "./EditIssueButton";
+import IssueDetails from "./IssueDetails";
+import DeleteIssueButton from "./DeleteIssueButton";
 
 interface Props {
-    params: { id: string}
+  params: { id: string };
 }
 
-const  IssueDetailPage = async ({ params }: Props) => {
-    // if (typeof params.id !== 'number') notFound();
- 
-    const issue = await prisma.issue.findUnique({
-        where: { id: parseInt(params.id) }
-    })
+const IssueDetailPage = async ({ params }: Props) => {
+  // if (typeof params.id !== 'number') notFound();
 
-    if (!issue)
-        notFound();
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
 
-
-
+  if (!issue) notFound();
 
   return (
-    <Grid columns={{ initial:"1", md: "2"}} gap={'5'}>
-        <Box>
-        <IssueDetails issue={issue}/>
-        </Box>
-        <Box>
-            <EditIssueButton issueId={issue.id}/>
-        </Box>
+    <Grid columns={{ initial: "1", sm: "5" }} gap={"5"}>
+      <Box className="md:col-span-4">
+        <IssueDetails issue={issue} />
+      </Box>
+      <Box>
+        <Flex gap="4" direction="column">
+          <EditIssueButton issueId={issue.id} />
+          <DeleteIssueButton issueId={issue.id} />
+        </Flex>
+      </Box>
     </Grid>
-  )
-}
+  );
+};
 
-export default IssueDetailPage
+export default IssueDetailPage;
