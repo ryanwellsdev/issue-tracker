19bce6c4f3eb87cefed7222b59e740208bd5b3bd
ryanwellsdev <ryanwellsdev@gmail.com>
2024-06-13 08:29:51 -0600

    remove duplicate skeletons

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,34 +1,40 @@
-'use client'
+"use client";
 
-import Link from 'next/link'
-import { usePathname } from 'next/navigation';
-import React from 'react'
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
 import { ImBug } from "react-icons/im";
-import classNames from 'classnames';
-
+import classNames from "classnames";
 
 const NavBar = () => {
+  const pathName = usePathname();
 
-    const pathName = usePathname()
-
-    const links = [
-        { label: 'Dashboard', href: '/'},
-        { label: 'Issues', href: '/issues'}
-    ] 
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues/list" },
+  ];
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
-        <Link href="/"><ImBug /></Link>
-        <ul className="flex space-x-6">
-            {links.map(link => <Link key={link.href} 
+      <Link href="/">
+        <ImBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <Link
+            key={link.href}
             className={classNames({
-                'text-zinc-900': link.href === pathName,
-                'text-zinc-500': link.href !== pathName,
-                'hover:text-zinc-800 transition-colors': true
-            })} 
-            href={link.href}>{link.label}</Link>)}
-        </ul>
+              "text-zinc-900": link.href === pathName,
+              "text-zinc-500": link.href !== pathName,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
