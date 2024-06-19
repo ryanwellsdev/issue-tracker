529cf4b86ddd59fb08bd4a2733a3a273d64a6f41
ryanwellsdev <ryanwellsdev@gmail.com>
2024-06-19 09:05:46 -0600

    Assign an issue to a user

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -30,7 +30,7 @@ const IssueDetailPage = async ({ params }: Props) => {
       {session && (
         <Box>
           <Flex gap="4" direction="column">
-            <AssigneeSelect />
+            <AssigneeSelect issue={issue} />
             <EditIssueButton issueId={issue.id} />
             <DeleteIssueButton issueId={issue.id} />
           </Flex>
