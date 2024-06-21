dde0784d7ffb8d13394ce7eff167f012dab4fe51
ryanwellsdev <ryanwellsdev@gmail.com>
2024-06-20 20:04:34 -0600

    Filter issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -2,9 +2,23 @@ import prisma from "@/prisma/client";
 import { Table } from "@radix-ui/themes";
 import { IssueStatusBadge, Link } from "@/app/components";
 import IssueToolbar from "./IssueToolbar";
+import { Status } from "@prisma/client";
 
-const IssuesPage = async () => {
-  const issue = await prisma.issue.findMany();
+interface Props {
+  searchParams: { status: Status };
+}
+
+const IssuesPage = async ({ searchParams }: Props) => {
+  const statuses = Object.values(Status);
+  const status = statuses.includes(searchParams.status)
+    ? searchParams.status
+    : undefined;
+  console.log(statuses);
+  const issue = await prisma.issue.findMany({
+    where: {
+      status,
+    },
+  });
 
   return (
     <div>
