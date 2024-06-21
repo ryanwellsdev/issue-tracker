d30d77a778ce299a302044d816ecd4e24cb9a644
ryanwellsdev <ryanwellsdev@gmail.com>
2024-06-20 19:41:04 -0600

    Build the filter component

--- app/issues/list/IssueToolbar.tsx
+++ app/issues/list/IssueToolbar.tsx
@@ -1,17 +1,16 @@
-import { Button } from '@radix-ui/themes'
-import Link from 'next/link'
-import React from 'react'
+import { Button, Flex } from "@radix-ui/themes";
+import Link from "next/link";
+import IssueStatusFilter from "./IssueStatusFilter";
 
 const IssueToolbar = () => {
   return (
-    <>
-    <div className='mb-5'>
-        <Button>
+    <Flex mb="5" justify={"between"}>
+      <IssueStatusFilter />
+      <Button>
         <Link href="/issues/new">New Issue</Link>
-        </Button>
-    </div>
-    </>
-  )
-}
+      </Button>
+    </Flex>
+  );
+};
 
-export default IssueToolbar
+export default IssueToolbar;
